28f80f229ea3037a4296a9c4d96d1415e346bcb0
Shafqat Ali <shafqat182404@gmail.com>
2024-01-27 23:24:19 +0500

    Define door class prototype

---
+++ door.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Door {
+    private:
+    bool isopen;
+    bool isLocked;
+public:
+    Door(); 
+    void openDoor();
+    void closeDoor();
+    void lockDoor();
+    void unlock();
+    bool isDoorOpen() const;
+    bool isDoor Locked() const;
+};
+int main(){
+return 0;
+}
